41c615859fb2f418358e397144ce6e46cca33311
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-05-22 03:10:09 +0530

    Add container folder

---
+++ src/container/index.jsx
@@ -0,0 +1,5 @@
+export { default as HeroContainer } from './HeroContainer/index';
+export { default as ServicesContainer } from './ServicesContainer/index';
+export { default as AboutUsContainer } from './AboutUsContainer/index';
+export { default as ClientsContainer } from './ClientsContainer/index';
+export { default as ContactUsContainer } from './ContactUsContainer/index';
